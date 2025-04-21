6087a98dd9f6c20ae5f1e2066db6276f661baec9
Aziz-Ennaciri <azizennaciri0@gmail.com>
2025-04-21 21:08:17 +0100

    first commit

---
+++ src/main/java/com/LiqaaTech/LiqaaTechApplication.java
@@ -0,0 +1,13 @@
+package com.LiqaaTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiqaaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiqaaTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LiqaaTech/LiqaaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LiqaaTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiqaaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
