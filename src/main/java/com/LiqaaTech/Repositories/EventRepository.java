5bf654388522309e7ba18e5f5a6a7ec78fbe5ba5
Aziz-Ennaciri <azizennaciri0@gmail.com>
2025-04-21 22:54:49 +0100

    Adding Repositories to each Entity

---
+++ src/main/java/com/LiqaaTech/Repositories/EventRepository.java
@@ -0,0 +1,9 @@
+package com.LiqaaTech.Repositories;
+
+import com.LiqaaTech.Entities.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event,Long> {
+}
