8b053ded5dc4b0a31678766d3472b71499a2c51d
Aziz-Ennaciri <azizennaciri0@gmail.com>
2025-04-27 01:23:32 +0100

    updating TicketMapper

--- src/main/java/com/LiqaaTech/Mappers/TicketMapper.java
+++ src/main/java/com/LiqaaTech/Mappers/TicketMapper.java
@@ -2,18 +2,34 @@ package com.LiqaaTech.Mappers;
 
 import com.LiqaaTech.DTOs.TicketDTO;
 import com.LiqaaTech.Entities.Ticket;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class TicketMapper {
+    @Autowired
+    private RegistrationMapper registasMapper;
+
     public TicketDTO toDTO(Ticket ticket) {
         TicketDTO ticketDTO = new TicketDTO();
         ticketDTO.setTicketCode(ticket.getTicketCode());
+        ticketDTO.setRegistrationDTO(registasMapper.toDTO(ticket.getRegistration()));
         return ticketDTO;
     }
     public Ticket toEntity(TicketDTO ticketDTO) {
         Ticket ticket = new Ticket();
         ticket.setTicketCode(ticketDTO.getTicketCode());
+        ticket.setRegistration(registasMapper.toEntity(ticketDTO.getRegistrationDTO()));
         return ticket;
     }
+
+    public List<TicketDTO> toDTOList(List<Ticket> tickets) {
+        return tickets.stream().map(this::toDTO).toList();
+    }
+    public List<Ticket> toEntityList(List<TicketDTO> ticketDTOS) {
+        return ticketDTOS.stream().map(this::toEntity).toList();
+    }
+
 }
