7902f9aef5dcdefeaa1c0066cf929a39284514b7
Aziz-Ennaciri <azizennaciri0@gmail.com>
2025-04-27 01:24:15 +0100

    creating CURD methods for TicketServiceImpl and add method for TicketRepository"findByEventIdAndRegistrationId"

--- src/main/java/com/LiqaaTech/Services/Impl/TicketServiceImpl.java
+++ src/main/java/com/LiqaaTech/Services/Impl/TicketServiceImpl.java
@@ -1,30 +1,47 @@
 package com.LiqaaTech.Services.Impl;
 
 import com.LiqaaTech.DTOs.TicketDTO;
+import com.LiqaaTech.Entities.Ticket;
+import com.LiqaaTech.Mappers.TicketMapper;
+import com.LiqaaTech.Repositories.TicketRepository;
 import com.LiqaaTech.Services.Interf.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TicketServiceImpl implements TicketService {
+    @Autowired
+    private TicketRepository ticketRepository;
+    @Autowired
+    private TicketMapper ticketMapper;
+
     @Override
     public TicketDTO createTicket(TicketDTO ticketDTO) {
-        return null;
+        Ticket ticket = ticketMapper.toEntity(ticketDTO);
+        Ticket savedTicket = ticketRepository.save(ticket);
+        return ticketMapper.toDTO(savedTicket);
     }
 
     @Override
     public List<TicketDTO> getAllTickets(Long eventId, Long registrationId) {
-        return null;
+        List<Ticket> tickets = ticketRepository.findByEventIdAndRegistrationId(eventId, registrationId);
+        if(tickets.isEmpty()) {
+            throw new RuntimeException("No tickets found");
+        }
+        return ticketMapper.toDTOList(tickets);
     }
 
     @Override
     public TicketDTO getTicketById(Long ticketId) {
-        return null;
+        Ticket ticket = ticketRepository.findById(ticketId).orElseThrow(() -> new RuntimeException("Ticket not found with id: " + ticketId));
+        return ticketMapper.toDTO(ticket);
     }
 
     @Override
     public void deleteTicket(Long ticketId) {
-
+        Ticket ticket = ticketRepository.findById(ticketId).orElseThrow(() -> new RuntimeException("Ticket not found with id: " + ticketId));
+        ticketRepository.delete(ticket);
     }
 }
