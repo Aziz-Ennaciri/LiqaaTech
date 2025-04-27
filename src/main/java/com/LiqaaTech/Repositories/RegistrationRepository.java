26f840d3bb103e9db9732690bbf2262e1bfa6e1e
Aziz-Ennaciri <azizennaciri0@gmail.com>
2025-04-27 01:49:33 +0100

    creating CRUD methods for RegistrationServiceImpl and add method "findByEventId" in RegistrationRepository

--- src/main/java/com/LiqaaTech/Repositories/RegistrationRepository.java
+++ src/main/java/com/LiqaaTech/Repositories/RegistrationRepository.java
@@ -4,6 +4,9 @@ import com.LiqaaTech.Entities.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
+    List<Registration> findByEventId(Long eventId);
 }
--- src/main/java/com/LiqaaTech/Services/Impl/RegistrationServiceImpl.java
+++ src/main/java/com/LiqaaTech/Services/Impl/RegistrationServiceImpl.java
@@ -1,35 +1,72 @@
 package com.LiqaaTech.Services.Impl;
 
 import com.LiqaaTech.DTOs.RegistrationDTO;
+import com.LiqaaTech.Entities.Registration;
+import com.LiqaaTech.Exceptions.NotFoundException;
+import com.LiqaaTech.Mappers.EventMapper;
+import com.LiqaaTech.Mappers.RegistrationMapper;
+import com.LiqaaTech.Mappers.TicketMapper;
+import com.LiqaaTech.Mappers.UserMapper;
+import com.LiqaaTech.Repositories.RegistrationRepository;
 import com.LiqaaTech.Services.Interf.RegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class RegistrationServiceImpl implements RegistrationService {
+    @Autowired
+    private RegistrationRepository registrationRepository;
+    @Autowired
+    private RegistrationMapper registrationMapper;
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private EventMapper eventMapper;
+    @Autowired
+    private TicketMapper ticketMapper;
+
     @Override
     public RegistrationDTO createRegistration(RegistrationDTO registrationDTO) {
-        return null;
+        Registration registration = registrationMapper.toEntity(registrationDTO);
+        Registration savedRegistration = registrationRepository.save(registration);
+        return registrationMapper.toDTO(savedRegistration);
     }
 
     @Override
     public List<RegistrationDTO> getAllRegistrations(Long eventId) {
-        return null;
+        List<Registration> registrations = registrationRepository.findByEventId(eventId);
+        if(registrations.isEmpty()) {
+            throw new NotFoundException("No registrations found");
+        }
+        return registrationMapper.toDTOList(registrations);
     }
 
     @Override
     public RegistrationDTO getRegistrationById(Long eventId, Long registrationId) {
-        return null;
+        Registration registration = registrationRepository.findById(registrationId)
+                .orElseThrow(() -> new NotFoundException("Registration not found"));
+        return registrationMapper.toDTO(registration);
     }
 
     @Override
     public RegistrationDTO updateRegistration(Long registrationId, RegistrationDTO registrationDTO) {
-        return null;
+        Registration registration = registrationRepository.findById(registrationId)
+                .orElseThrow(() -> new NotFoundException("Registration not found"));
+        registration.setParticipant(userMapper.toEntity(registrationDTO.getParticipantDTO()));
+        registration.setEvent(eventMapper.toEntity(registrationDTO.getEventDTO()));
+        registration.setRegisteredAt(registrationDTO.getRegisteredAt());
+        registration.setInWaitingList(registrationDTO.isInWaitingList());
+        registration.setTicket(ticketMapper.toEntity(registrationDTO.getTicketDTO()));
+        Registration updatedRegistration = registrationRepository.save(registration);
+        return registrationMapper.toDTO(updatedRegistration);
     }
 
     @Override
     public void deleteRegistration(Long registrationId) {
-
+        Registration registration = registrationRepository.findById(registrationId)
+                .orElseThrow(() -> new NotFoundException("Registration not found"));
+        registrationRepository.delete(registration);
     }
 }
