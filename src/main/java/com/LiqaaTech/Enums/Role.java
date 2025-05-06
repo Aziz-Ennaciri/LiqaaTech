1e8a85ea808bdef9c6d6e3e339676e34aac8ef62
Aziz-Ennaciri <azizennaciri0@gmail.com>
2025-05-06 23:40:49 +0100

    update Role enum for specification

--- src/main/java/com/LiqaaTech/Enums/Role.java
+++ src/main/java/com/LiqaaTech/Enums/Role.java
@@ -1,5 +1,25 @@
 package com.LiqaaTech.Enums;
 
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
 public enum Role {
-    PARTICIPANT,ORGANIZER, ADMIN
+    PARTICIPANT(Set.of(Permission.READ_EVENTS, Permission.REGISTER_EVENT)),
+    ORGANIZER(Set.of(Permission.READ_EVENTS, Permission.REGISTER_EVENT, Permission.CREATE_EVENT, Permission.UPDATE_EVENT, Permission.DELETE_EVENT)),
+    ADMIN(Set.of(Permission.values()));
+
+    private final Set<Permission> permissions;
+
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + this.name()));
+    }
 }
